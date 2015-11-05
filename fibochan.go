b2325ddac4ea9771bf6b17568eee5e368ace5a2a
Luciano Ramalho <luciano@ramalho.org>
2015-11-05 07:23:18 -0200

    example goroutine generating Fibonacci series over a channel

---
+++ fibochan.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func fib(n int, results chan<- uint64) {
+	a, b := uint64(0), uint64(1)
+	results <- a
+	for n > 0 {
+		if b < a {
+			panic(fmt.Sprintf("fib() overflow: a > b (%d > %d)", a, b))
+		}
+		a, b = b, a+b
+		results <- a
+		n--
+	}
+	close(results)
+}
+
+func parseArg() (n int) {
+	var err error
+	if len(os.Args) == 2 {
+		n, err = strconv.Atoi(os.Args[1])
+		if err == nil && n < 0 {
+			err = errors.New("Argument must be n >= 0")
+		}
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+	if err != nil || len(os.Args) != 2 {
+		fmt.Println("Usage:  fibochan <n>")
+		os.Exit(1)
+	}
+	return n
+}
+
+func main() {
+	n := parseArg()
+	results := make(chan uint64)
+	go fib(n, results)
+	i := 0
+	for r := range results {
+		fmt.Printf("%3d: %d\n", i, r)
+		i++
+	}
+}
